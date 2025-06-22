01917fd39249eb37f049cf4235e598f4b721c8e5
Adil Muustafa <dev.adil786@gmail.com>
2025-06-22 20:19:22 +0500

    Add CORS middleware in server.js to allow all origins and specify allowed methods and headers for improved API flexibility.

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use(express.json());
 
 // MongoDB Connection
